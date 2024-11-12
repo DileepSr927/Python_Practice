ea7fb96f2cb58403e90a4e6955daf8e4cab9dbc9
DileepSr927 <pdileepkumar1807@gmail.com>
2024-11-12 17:40:53 +0530

    12 Nov 2024

---
+++ Nov/ex_12112024_Python_Operators/task001_Python_Operators.py
@@ -0,0 +1,6 @@
+user=int(input("enter age\n"))
+
+if user>21:
+    print("Yes your enter the club")
+else:
+    print("no not allowed the club")
---
+++ Nov/ex_12112024_Python_Operators/task002_Python_Operators.py
@@ -0,0 +1,6 @@
+a=int(input("enter a value\n"))
+b=int(input("enter b value\n"))
+if a>b:
+    print("a value is big")
+else:
+    print("b value is big")
